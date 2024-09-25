0038bdc9505b32b426ab803087ec1dcccf17fa2e
Daniel24-dev <odintor18@gmail.com>
2024-09-26 04:51:38 +0900

    Update Main.java
    added Skeleton 2 exemplar

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
         Boss boss = new Boss(200,34,weapon);
         Skeleton lich = new Skeleton(10,3,new Weapon(WeaponType.BOW,"Frozen Bow"),66);
+        Skeleton lich1 = new Skeleton(9,2,new Weapon(WeaponType.BOW,"Fire Bow"),70);
         boss.printInfo();
         lich.printInfo();
     }
